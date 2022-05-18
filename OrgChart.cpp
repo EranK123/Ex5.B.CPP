bb0273ba3245e501c8e93caa8cb7401fa68d2668
erankatz <eranki.katz@gmail.com>
2022-05-18 10:44:54 +0300

    need reverse

--- OrgChart.cpp
+++ OrgChart.cpp
@@ -4,46 +4,109 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include<algorithm>
 using namespace std;
 using namespace ariel;
 
-OrgChart::OrgChart(){
 
+OrgChart::OrgChart(){
+    this->first = nullptr;
+}
+
+OrgChart::~OrgChart(){
+    delete(this->first);
 }
 
 OrgChart& OrgChart::add_root(string position){
-return *this;
+    this->first = newNode(position);
+    return *this;
 }
 
 OrgChart& OrgChart::add_sub(string higherPosName, string lowerPosName){
-return *this;
-}
+    Node *new_node = newNode(lowerPosName); //create new node
+    if(higherPosName == this->first->data){ //if its the root add it
+    this->first->subs.push_back(new_node);   
+    }
+    Node *temp = this->first;
+    for(size_t i = 0; i < temp->subs.size(); i++){
+        if(temp->subs.at(i)->data == higherPosName){
+            temp->subs.at(i)->subs.push_back(new_node);
+            break;
+        }  
+    }
+    return *this;
+    }
 
 ostream& ariel::operator<<(ostream& output, const OrgChart &org){
-return output;
+	queue<OrgChart::Node *> q; // Create a queue
+	q.push(org.first); // Enqueue root
+	while (!q.empty())
+	{
+		int n = q.size();
+
+		// If this node has children
+		while (n > 0)
+		{
+			// Dequeue an item from queue and print it
+			OrgChart::Node * p = q.front();
+			q.pop();
+			output << p->data << " ";
+
+			// Enqueue all children of the dequeued item
+			for (int i=0; i<p->subs.size(); i++){
+				q.push(p->subs.at(i));
+            }
+			n--;
+		}
+
+		output << endl; // Print new line between two levels
+	}
+    return output;
 }
 
 OrgChart::Iterator OrgChart::begin(){
-    return OrgChart::Iterator();
+    return Iterator(this->first, 0);
 }
 OrgChart::Iterator OrgChart::end(){
-    return OrgChart::Iterator();
+    return Iterator(nullptr, 0);
 }
 OrgChart::Iterator OrgChart::begin_level_order(){
-    return OrgChart::Iterator();
+    return Iterator(this->first, 0);
 }
 OrgChart::Iterator OrgChart::end_level_order(){
-    return OrgChart::Iterator();
+    return Iterator(nullptr, 0);
 }
 OrgChart::Iterator OrgChart::begin_reverse_order(){
-    return OrgChart::Iterator();
+    return Iterator(nullptr , 1);
 }
 OrgChart::Iterator OrgChart::reverse_order(){
-    return OrgChart::Iterator();
+    return Iterator(this->first, 1);
 }
 OrgChart::Iterator OrgChart::begin_preorder(){
-    return OrgChart::Iterator();
+    return Iterator(this->first, 2);
 }
 OrgChart::Iterator OrgChart::end_preorder(){
-    return OrgChart::Iterator();
+    return Iterator(nullptr, 2);
+}
+
+int main(){
+    OrgChart org;
+    org.add_root("CEO")
+      .add_sub("CEO", "CTO")        // Now the CTO is subordinate to the CEO
+      .add_sub("CEO", "CFO")        // Now the CFO is subordinate to the CEO         // Now the COO is subordinate to the CEO
+      .add_sub("CTO", "VP_SW") // Now the VP Software is subordinate to the CTO
+      .add_sub("CEO", "COO")
+        .add_sub("COO", "VP_BI");
+    for (auto it = org.begin_preorder(); it != org.end_preorder(); ++it)
+  {
+    cout << (*it) << " " ;
+  }
+// for (auto element : org)
+//   { // this should work like level order
+//     cout << element << " " ;
+//   } // 
+//    / prints: CEO CTO CFO COO VP_SW VP_BI
+   
+// cout << org;
+
 }
--- OrgChart.hpp
+++ OrgChart.hpp
@@ -2,53 +2,114 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 using namespace std;
 namespace ariel{
     class OrgChart{
-        private:
-       struct Node{
-            string position;
-            // vector<string> s;
-            Node *next;
-            Node(const string &pos, Node* n){
-                position = pos;
-                next = n;
-            }
-        };
+       private:
+      
         
         public:
+         struct Node{
+            string data;
+            vector<Node *> subs;
+
+        };
+        Node *newNode(string data){
+        Node *temp = new Node;
+        temp->data = data;
+        return temp;
+        }
+        Node *first;
         OrgChart();
+        ~OrgChart();
         OrgChart& add_root(string position);
         OrgChart& add_sub(string higherPosName, string lowerPosName);
         friend ostream& operator<<(ostream& output, const OrgChart &org);
     class Iterator{
       private:
-      uint index;
-      Node *currentNode;
+      int curr_index;
+      vector<Node*> tree_list;
       public:
-      Iterator(){
-          this->index = 0;
-          this->currentNode = NULL;
+
+    void level_order(Node* root){
+    queue<Node *> q; // Create a queue
+    q.push(root); // Enqueue root
+    while (!q.empty())
+    {
+        int n = q.size();
+
+        while (n > 0)
+        {
+            Node * p = q.front();
+            q.pop();
+            this->tree_list.push_back(p);
+            for (int i=0; i<p->subs.size(); i++){
+                q.push(p->subs.at(i));
+            }
+            n--;
+        }
+    }  
+}
+
+      void reverse_order(Node *root){
+
       }
-    //   Iterator(Node* ptr = nullptr){
-    //       this->currentNode = ptr;
-    //   }
+
+      void preorder(Node *root){
+        stack<Node*> s;
+        vector<string> orderNodes;
+        s.push(root);
+        while (!s.empty()) {
+        Node* temp = s.top();
+        s.pop();
+        // store the key in preorder vector(visited list)
+        this->tree_list.push_back(temp);
+        // Push all of the child nodes of temp into
+        // the stack from right to left.
+        for (int i = temp->subs.size() - 1; i >= 0; i--) {
+            s.push(temp->subs.at(i));
+        }
+        }
+}
+
+      Iterator(Node* ptr, int traverse_way){
+          if(traverse_way == 0 && ptr != nullptr){
+              level_order(ptr);
+          }else if(traverse_way == 1 && ptr != nullptr){
+              reverse_order(ptr);
+          }else{
+              if(ptr != nullptr){
+                  preorder(ptr);
+              }
+          }
+          this->curr_index = 0;
+          this->tree_list.push_back(nullptr);
+      }
       bool operator!=(const Iterator& it) const{
-          return false;
-      }
+            return this->tree_list[this->curr_index] != it.tree_list[it.curr_index];
+    }
+        
      const Iterator operator++(int){
          Iterator t = *this;
+         this->curr_index++;
           return t;
       }
       Iterator& operator++(){
+          ++this->curr_index;
           return *this;
       }
       string operator*() const{
-          return "";
+          return this->tree_list[curr_index]->data;
       }
       string* operator->() const{
-          return nullptr;
+          return &(this->tree_list[curr_index]->data);
       }
+
+    // bool operator==(const Iterator &it) const {
+    //         return !(*this != it);
+    //     }
 
     };
     Iterator begin();
